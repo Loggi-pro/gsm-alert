5ab374a3dfc0ee43890e6c567bf7079ea4f20ed1
loggi <loggi.ain@gmail.com>
2021-01-06 02:40:52 +1000

    refactor business logic

--- src/algorithm.rs
+++ src/algorithm.rs
@@ -2,54 +2,17 @@ use crate::button::Button;
 use crate::door_sensor::{DoorSensor, DoorState};
 use crate::indication::{Indication, IndicationState};
 use crate::sim900::Sim900;
-use crate::timer::{CounterTypeExt, TimeType, Timer};
-pub struct MainLogic {
+use crate::timer::{CounterTypeExt, Timer};
+
+struct Resources {
     sim900: Sim900,
     indication: Indication,
     power_button: Button,
     door_sensor: DoorSensor,
-    current_state: IndicationState,
-    is_error: bool,
-    timer: Timer,
-    timer2: Timer,
     check_state: u8,
-    flag_go_check: bool,
 }
 
-fn synchronize<T: Sized>(mut f: impl FnMut() -> Option<T>) -> T {
-    loop {
-        if let Some(x) = f() {
-            return x;
-        }
-    }
-}
-
-impl MainLogic {
-    pub fn new(
-        sim900: Sim900,
-        indication: Indication,
-        power_button: Button,
-        door_sensor: DoorSensor,
-    ) -> MainLogic {
-        MainLogic {
-            sim900,
-            indication,
-            power_button,
-            door_sensor,
-            current_state: IndicationState::Idle,
-            is_error: false,
-            timer: Timer::new(),
-            timer2: Timer::new(),
-            check_state: 0,
-            flag_go_check: false,
-        }
-    }
-    fn delay(&mut self) -> Option<bool> {
-        if self.timer2.every(3.sec()) {
-            return Some(true);
-        }
-        return None;
-    }
+impl Resources {
     fn check_gsm(&mut self) -> Option<bool> {
         match self.check_state {
             0 => {
@@ -89,97 +52,228 @@ impl MainLogic {
             }
         }
     }
-    fn set_error(&mut self) {
-        self.is_error = true;
-        self.indication.set_state(IndicationState::Error);
+}
+pub struct MainLogic {
+    resources: Resources,
+    current_state: AlgorithmState,
+}
+
+fn synchronize<T: Sized>(mut f: impl FnMut() -> Option<T>) -> T {
+    loop {
+        if let Some(x) = f() {
+            return x;
+        }
     }
-    fn clear_error(&mut self) {
-        self.is_error = false;
-        self.indication.set_state(self.current_state);
+}
+
+enum AlgorithmState {
+    IdleState(Idle),
+    CheckState(Check),
+    ReadyToArmState(ReadyToArm),
+    ArmedState(Armed),
+    ErrorState(Error),
+}
+
+impl MainLogic {
+    pub fn new(
+        sim900: Sim900,
+        indication: Indication,
+        power_button: Button,
+        door_sensor: DoorSensor,
+    ) -> MainLogic {
+        MainLogic {
+            resources: Resources {
+                sim900,
+                indication,
+                power_button,
+                door_sensor,
+                check_state: 0,
+            },
+            current_state: AlgorithmState::IdleState(Idle {}),
+        }
     }
+
+    fn update_view(&mut self) {
+        let new_view_state = match self.current_state {
+            AlgorithmState::IdleState(_) => IndicationState::Idle,
+            AlgorithmState::CheckState(_) => IndicationState::CheckBeforeArm,
+            AlgorithmState::ReadyToArmState(_) => IndicationState::ReadyToArm,
+            AlgorithmState::ArmedState(_) => IndicationState::Armed,
+            AlgorithmState::ErrorState(_) => IndicationState::Error,
+        };
+        self.resources.indication.set_state(new_view_state);
+    }
+
     pub fn init(&mut self) {
-        if !synchronize(|| self.check_gsm()) {
-            self.set_error();
-        } else {
-            self.current_state = match self.door_sensor.is_closed() {
-                true => IndicationState::Armed,
-                false => IndicationState::Idle,
-            };
-            self.clear_error();
+        if let AlgorithmState::IdleState(x) = self.current_state {
+            self.current_state = x.init(&mut self.resources);
+            self.update_view();
         }
     }
-    fn button_poll(&mut self) {
-        if self.is_error {
-            return;
-        }
-        if let Some(true) = self.power_button.is_pressed() {
-            self.current_state = match self.current_state {
-                IndicationState::Armed
-                | IndicationState::ReadyToArm
-                | IndicationState::CheckBeforeArm => IndicationState::Idle,
-                IndicationState::Idle => IndicationState::CheckBeforeArm,
-                _ => self.current_state,
-            };
-            self.indication.set_state(self.current_state);
-        }
-    }
-    fn state_arm(&mut self) {
-        self.current_state = IndicationState::Armed;
-        self.indication.set_state(self.current_state);
-    }
-    fn state_disarm(&mut self) {
-        self.current_state = IndicationState::Idle;
-        self.indication.set_state(self.current_state);
-    }
-    fn door_poll(&mut self) {
-        match self.current_state {
-            IndicationState::ReadyToArm => {
-                if let Some(DoorState::Closed) = self.door_sensor.state() {
-                    self.state_arm();
-                }
-            }
-            IndicationState::Armed => {
-                if let Some(DoorState::Opened) = self.door_sensor.state() {
-                    self.state_disarm();
-                }
-            }
-            _ => (),
-        }
-    }
-
-    fn gsm_poll<T: TimeType>(&mut self, period: T) {
-        //check gsm every 30 sec on error, or only before arm
-        match self.current_state {
-            IndicationState::CheckBeforeArm => self.flag_go_check = true,
-            IndicationState::Error => {
-                if self.timer.every(period) {
-                    self.flag_go_check = true
-                }
-            }
-            _ => (),
-        }
-        if !self.flag_go_check {
-            return;
-        }
-        if let Some(gsm_good) = self.check_gsm() {
-            //on check result
-            self.flag_go_check = false;
-            //if was check mode before arm then set ready
-            if self.current_state == IndicationState::CheckBeforeArm && gsm_good {
-                self.current_state = IndicationState::ReadyToArm;
-            }
-            //from other mode=> go to error or previous mode
-            match gsm_good {
-                true => self.clear_error(),
-                false => self.set_error(),
-            };
-        }
-    }
-
     pub fn poll(&mut self) {
-        self.indication.poll();
-        self.button_poll();
-        self.door_poll();
-        self.gsm_poll(30.sec());
+        self.resources.indication.poll();
+        if let Some(x) = self.current_state.poll(&mut self.resources) {
+            self.current_state = x;
+            self.update_view();
+        }
+    }
+}
+
+struct Check {}
+struct Armed {}
+struct ReadyToArm {}
+struct Error {
+    timer: Timer,
+    from_state: IndicationState,
+    flag_go_check: bool,
+}
+#[derive(Copy, Clone)]
+struct Idle {}
+impl Idle {
+    pub fn init(self, resources: &mut Resources) -> AlgorithmState {
+        if !synchronize(|| resources.check_gsm()) {
+            return AlgorithmState::ErrorState(Error::new(IndicationState::Idle));
+        }
+        match resources.door_sensor.is_closed() {
+            true => AlgorithmState::ArmedState(Armed {}),
+            false => AlgorithmState::IdleState(self),
+        }
+    }
+    fn button_poll(&self, resources: &mut Resources) -> Option<AlgorithmState> {
+        if let Some(true) = resources.power_button.is_pressed() {
+            Some(AlgorithmState::CheckState(Check {}))
+        } else {
+            None
+        }
+    }
+    fn poll(&mut self, resources: &mut Resources) -> Option<AlgorithmState> {
+        self.button_poll(resources)
+    }
+}
+
+impl Check {
+    fn button_poll(&self, resources: &mut Resources) -> Option<AlgorithmState> {
+        if let Some(true) = resources.power_button.is_pressed() {
+            Some(AlgorithmState::IdleState(Idle {}))
+        } else {
+            None
+        }
+    }
+    fn gsm_poll(&self, resources: &mut Resources) -> Option<AlgorithmState> {
+        if let Some(gsm_good) = resources.check_gsm() {
+            return match gsm_good {
+                true => Some(AlgorithmState::ReadyToArmState(ReadyToArm {})),
+                false => Some(AlgorithmState::ErrorState(Error::new(
+                    IndicationState::ReadyToArm,
+                ))),
+            };
+        }
+        None
+    }
+    fn poll(&mut self, resources: &mut Resources) -> Option<AlgorithmState> {
+        if let Some(x) = self.button_poll(resources) {
+            return Some(x);
+        }
+        if let Some(x) = self.gsm_poll(resources) {
+            return Some(x);
+        }
+        return None;
+    }
+}
+impl Error {
+    pub fn new(from_state: IndicationState) -> Self {
+        Self {
+            timer: Timer::new(),
+            from_state,
+            flag_go_check: false,
+        }
+    }
+    fn gsm_poll(&mut self, resources: &mut Resources) -> Option<AlgorithmState> {
+        //check gsm every 30 sec on error
+        if self.timer.every(30.sec()) {
+            self.flag_go_check = true
+        }
+
+        if !self.flag_go_check {
+            return None;
+        }
+        if let Some(gsm_good) = resources.check_gsm() {
+            if !gsm_good {
+                return None;
+            }
+            return match self.from_state {
+                IndicationState::CheckBeforeArm => Some(AlgorithmState::CheckState(Check {})),
+                IndicationState::ReadyToArm => Some(AlgorithmState::ReadyToArmState(ReadyToArm {})),
+                IndicationState::Armed => Some(AlgorithmState::ArmedState(Armed {})),
+                _ => Some(AlgorithmState::IdleState(Idle {})),
+            };
+        }
+        return None;
+    }
+
+    pub fn poll(&mut self, resources: &mut Resources) -> Option<AlgorithmState> {
+        self.gsm_poll(resources)
+    }
+}
+
+impl ReadyToArm {
+    fn button_poll(&mut self, resources: &mut Resources) -> Option<AlgorithmState> {
+        if let Some(true) = resources.power_button.is_pressed() {
+            return Some(AlgorithmState::IdleState(Idle {}));
+        }
+        None
+    }
+    fn door_poll(&mut self, resources: &mut Resources) -> Option<AlgorithmState> {
+        if let Some(DoorState::Closed) = resources.door_sensor.state() {
+            //do some actions
+            return Some(AlgorithmState::ArmedState(Armed {}));
+        }
+        None
+    }
+    fn poll(&mut self, resources: &mut Resources) -> Option<AlgorithmState> {
+        if let Some(x) = self.button_poll(resources) {
+            return Some(x);
+        }
+        if let Some(x) = self.door_poll(resources) {
+            return Some(x);
+        }
+        return None;
+    }
+}
+
+impl Armed {
+    fn button_poll(&mut self, resources: &mut Resources) -> Option<AlgorithmState> {
+        if let Some(true) = resources.power_button.is_pressed() {
+            return Some(AlgorithmState::IdleState(Idle {}));
+        }
+        None
+    }
+    fn door_poll(&mut self, resources: &mut Resources) -> Option<AlgorithmState> {
+        if let Some(DoorState::Opened) = resources.door_sensor.state() {
+            //do some actions
+            //smsing
+            return Some(AlgorithmState::IdleState(Idle {}));
+        }
+        None
+    }
+    fn poll(&mut self, resources: &mut Resources) -> Option<AlgorithmState> {
+        if let Some(x) = self.button_poll(resources) {
+            return Some(x);
+        }
+        if let Some(x) = self.door_poll(resources) {
+            return Some(x);
+        }
+        return None;
+    }
+}
+impl AlgorithmState {
+    fn poll(&mut self, resources: &mut Resources) -> Option<AlgorithmState> {
+        match self {
+            AlgorithmState::IdleState(x) => x.poll(resources),
+            AlgorithmState::CheckState(x) => x.poll(resources),
+            AlgorithmState::ReadyToArmState(x) => x.poll(resources),
+            AlgorithmState::ArmedState(x) => x.poll(resources),
+            AlgorithmState::ErrorState(x) => x.poll(resources),
+        }
     }
 }
